8d6ef72f0b08cbccd4d57f08dfc52fe6b2bc1606
Taylor Huffman <taylor@crucialartists.com>
2022-09-01 11:04:44 -0500

    Add login and logout feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
-  pose '/login', to: 's'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
