3db7ae8d46c0dd33f5b5ec5810cd14dcd0360823
Taylor Huffman <taylor@crucialartists.com>
2022-09-01 10:49:24 -0500

    Add signup and auto-login feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    before_action :authorize
+    skip_before_action :authorize, only: :create
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        render json: user
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+    end
+
+    def authorize
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
+    
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :recipes
-  resources :users
+  # resources :recipes
+  # resources :users
+
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  pose '/login', to: 's'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
